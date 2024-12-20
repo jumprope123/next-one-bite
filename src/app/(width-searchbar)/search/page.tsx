81cfa0b223dda3ba65cb6e35b6edeaec199b684b
ryan <ryan@2digit.io>
2024-12-20 10:08:44 +0900

    리퀘스트메모이제이션

--- src/app/(width-searchbar)/search/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Search({
-  searchParams,
-}: {
-  searchParams: Promise<{ q: string }>;
-}) {
-  const { q } = await searchParams;
-  console.log(searchParams);
-  return <div>search Page {q}</div>;
-}
--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,9 +1,37 @@
+import style from "./page.module.css";
+
 export default async function Page({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: { id: string | string[] };
 }) {
-  const { id } = await params;
-  console.log(id);
-  return <div>book/[{id}] page입니다.</div>;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${params.id}`
+  );
+
+  if (!response.ok) {
+    <div>오류발생</div>;
+  }
+
+  const book = await response.json();
+
+  const { id, title, subTitle, description, author, publisher, coverImgUrl } =
+    book;
+
+  return (
+    <div className={style.container}>
+      <div
+        className={style.cover_img_container}
+        style={{ backgroundImage: `url('${coverImgUrl}')` }}
+      >
+        <img src={coverImgUrl} />
+      </div>
+      <div className={style.title}>{title}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.author}>
+        {author} | {publisher}
+      </div>
+      <div className={style.description}>{description}</div>
+    </div>
+  );
 }
